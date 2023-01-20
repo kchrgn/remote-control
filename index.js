9384a6a7bc556830c5cb2f739a3acbb93321249b
Andrey S. Kochergin <kchrgn@gmail.com>
2023-01-20 19:41:30 +0300

    feat: implement show ws parameters on start app

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { httpServer } from "./src/http_server/index.js";
 import { mouse } from "@nut-tree/nut-js";
-import { initWSConnection } from "./src/ws/ws.js";
+import { WSServer } from "./src/ws/ws.js";
 
 const HTTP_PORT = 8181;
+const WSS_PORT = 8080;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
-initWSConnection();
+new WSServer(WSS_PORT);
+process.on('SIGINT', () => WSServer.close());
--- src/ws/ws.js
+++ src/ws/ws.js
@@ -1,9 +1,17 @@
 import { createWebSocketStream, WebSocketServer } from "ws";
 
-export const initWSConnection = () => {
-  const wss = new WebSocketServer({ port: 8080 });
-  wss.on('connection', (ws) => {
-    const backStream = createWebSocketStream(ws, { encoding: 'utf8' });
-    backStream.pipe(process.stdout);
-  })
+export class WSServer {
+  constructor (port) {
+    this.wss = new WebSocketServer({ port });
+    this.wss.on('connection', (ws) => {
+      const address = (ws._socket.address());
+      console.log(`Server WS parameters: port: ${address.port}, family: ${address.family}, address: ${address.address}`);
+      console.log(`Client WS parameters: port: ${ws._socket.remotePort}, address: ${ws._socket.remoteAddress}`);
+      const backStream = createWebSocketStream(ws, { encoding: 'utf8' });
+      backStream.pipe(process.stdout);
+    })
+  }
+  close() {
+    this.wss.close();
+  }
 }
